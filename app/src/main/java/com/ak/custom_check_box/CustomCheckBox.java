a25fc4e593126859f58d5190fc88d7efd689ea0a
Andrey Kushnaryov <akushnaryov@rightandabove.com>
2020-04-09 14:56:30 +0300

    added scale factor

--- app/src/main/java/com/ak/custom_check_box/CustomCheckBox.java
+++ app/src/main/java/com/ak/custom_check_box/CustomCheckBox.java
@@ -5,20 +5,30 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.LayerDrawable;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.widget.CompoundButton;
 
+import androidx.core.content.ContextCompat;
+
 public class CustomCheckBox extends androidx.appcompat.widget.AppCompatCheckBox {
-    //CustomCheckBox customCheckbox = (CustomCheckBox) findViewById(R.id.custom_CB2);
+    CustomCheckBox customCheckbox = (CustomCheckBox) findViewById(R.id.custom_CB2);
     Drawable tempDrawable = getResources().getDrawable(R.drawable.checked_custom).mutate();
     LayerDrawable bubble = (LayerDrawable) tempDrawable.mutate();
     GradientDrawable solidColor = (GradientDrawable) bubble.findDrawableByLayerId(R.id.shapeChecked).mutate();
+    //GradientDrawable solidColor2 = (GradientDrawable) bubble.findDrawableByLayerId(R.id.shapeCheckedItem2).mutate();
+    GradientDrawable solidColor2 = (GradientDrawable) bubble.findDrawableByLayerId(R.id.shapeChecked2).mutate();
+
+    int SIZE_XML_ITEM = 24;
+    int STROKE_DEFAULT_WIDTH = 4;
+    int STROKE2_DEFAULT_WIDTH = 2;
+    int unCheckedColor = 0;
+    int unCheckedColor2 = 0;
+
 
     public CustomCheckBox(Context context) {
         super(context);
         init();
     }
-
-
 
 
     public CustomCheckBox(Context context, AttributeSet attrs) {
@@ -32,13 +42,14 @@ public class CustomCheckBox extends androidx.appcompat.widget.AppCompatCheckBox 
     }
 
     private void init() {
+        this.setButtonDrawable(tempDrawable);
 
 
         //mySetColor(0xffFFFF00);
         this.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                mySetColor(0xFF1c1bb5);
+                mySetColor(0xFF4285f5);
 
             }
         });
@@ -50,10 +61,20 @@ public class CustomCheckBox extends androidx.appcompat.widget.AppCompatCheckBox 
         this.setLayoutDirection(LAYOUT_DIRECTION_RTL);
     }
 
-    public void mySetCheckBoxSize(int w, int h) {
 
+    public void mySetCheckBoxSize(int size) {
 
-        solidColor.setSize(w, h);
+        //color
+        solidColor.setSize(size, size);
+
+        int factor = size / SIZE_XML_ITEM;
+        // get color stroke item 1 and item2
+        unCheckedColor = ContextCompat.getColor(getContext(), R.color.unCheckedStrokeOne);
+        unCheckedColor2 = ContextCompat.getColor(getContext(), R.color.unCheckedStrokeTwo);
+
+        // set width and color item 1 and item2
+        solidColor.setStroke(STROKE_DEFAULT_WIDTH * factor, unCheckedColor);
+        solidColor2.setStroke(STROKE2_DEFAULT_WIDTH * factor, unCheckedColor2);
         this.setButtonDrawable(tempDrawable);
     }
 
@@ -64,6 +85,7 @@ public class CustomCheckBox extends androidx.appcompat.widget.AppCompatCheckBox 
             solidColor.setColor(intNewColorPos);
             this.setButtonDrawable(tempDrawable);
 
+
         } else {
             solidColor.setColor(0xFFffffff);
             this.setButtonDrawable(tempDrawable);
@@ -71,6 +93,24 @@ public class CustomCheckBox extends androidx.appcompat.widget.AppCompatCheckBox 
 
 
     }
+
+    //Utils
+    public static int dpToPx(float dp, Context context) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
+    }
+
+    public static int spToPx(float sp, Context context) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, context.getResources().getDisplayMetrics());
+    }
+
+    public static int dpToSp(float dp, Context context) {
+        return (int) (dpToPx(dp, context) / context.getResources().getDisplayMetrics().scaledDensity);
+    }
+
+    public static float pxToSp(float px, Context context) {
+        return (float) px / context.getResources().getDisplayMetrics().scaledDensity;
+    }
+
 }
 
 
--- app/src/main/java/com/ak/custom_check_box/MainActivity.java
+++ app/src/main/java/com/ak/custom_check_box/MainActivity.java
@@ -19,15 +19,25 @@ public class MainActivity extends AppCompatActivity {
 
 
         Button button = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkBox.setTextColor(Color.BLACK);
+                checkBox.mySetCheckBoxSize(50);
+            }
+        });
 
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 checkBox.setTextColor(Color.RED);
+                checkBox.mySetCheckBoxSize(200);
 //                checkBox.mySetColor(0xffFFFF00);
-                checkBox.mySetCheckBoxSize(100, 100);
-                checkBox.mySetLayoutDirectionLeft();
+                //checkBox.mySetCheckBoxSize(150);
+               // checkBox.mySetLayoutDirectionLeft();
 
 
             }
